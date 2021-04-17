d498c6ede33d8cfc8f1dabbe61d9c0cebeec309e
Dominik Hübner <dominikhubner@protonmail.com>
2021-04-18 00:30:11 +0200

    getting data from sql database instead of .json file

--- english_thesaurus.py
+++ english_thesaurus.py
@@ -1,40 +1,61 @@
-import json
 from difflib import get_close_matches
+import mysql.connector
 
-data = json.load(open("data.json"))
+connection = mysql.connector.connect(
+user = "ardit700_student", 
+password = "ardit700_student", 
+host = "108.167.140.122", 
+database = "ardit700_pm1database"
+)
+
+cursor = connection.cursor()
 
 def check_word(word):   
-    word_small = word.lower()
-    if word_small in data.keys():
-        return(word_small)
-    elif word_small.title() in data.keys(): 
-        return(word_small.title())
-    elif word_small.upper() in data.keys():
-        return(word_small.upper())
-    else:
-        sim_word_upper = get_close_matches(word_small.upper(), data.keys())[0]
-        sim_word_small = get_close_matches(word_small, data.keys())[0]
-        sim_word_capital = get_close_matches(word_small.title(), data.keys())[0]
-        sim_word = get_close_matches(word, [sim_word_capital, sim_word_small, sim_word_upper])
-        if len(sim_word) > 0:
-            while True:
-                user_new_word_y_n = input(f"Did you mean {sim_word[0]}?\n[yes] | [no] ")
-                if user_new_word_y_n == "yes":
-                    return(sim_word[0])
-                elif user_new_word_y_n == "no":
-                    print("Word not found")
-                    return 0
-                else:
-                    print('Type "yes" or "no"')
-        else:
-            print("Word not found")
-            return 0
+    word1 = word.lower()
+    word_list = [word1, word1.title(), word1.upper()]
 
-def definition(word):
-    n = 0
-    for i in data[word]:
-        print(data[word][n])
-        n = n+1
+    for i in word_list:
+        query = cursor.execute(f"SELECT * FROM Dictionary WHERE Expression = '{i}'")
+        results = cursor.fetchall()
+        
+        if results:
+            return results
+    
+    query1 = cursor.execute("SELECT Expression from Dictionary")
+    keys = [b[0] for b in cursor.fetchall()]
+    sim_word_list = []
+    
+    for i in word_list:
+        if len(get_close_matches(i, keys)) > 0:
+            sim_word_list.append(get_close_matches(i, keys)[0])
+
+    sim_word = get_close_matches(word, sim_word_list)
+
+    if len(sim_word) > 0:
+        while True:
+            user_new_word_y_n = input(f"Did you mean {sim_word[0]}?\n[yes] | [no] ")
+            
+            if user_new_word_y_n == "yes":
+                query = cursor.execute(f"SELECT * FROM Dictionary WHERE Expression = '{sim_word[0]}'")
+                results = cursor.fetchall()
+                return results
+                
+            elif user_new_word_y_n == "no":
+                print("Word not found")
+                return 0
+                
+            else:
+                print('Type "yes" or "no"')
+        
+    else:
+        print("Word not found")
+        return 0
+
+    
+
+def definition(results):
+    for result in results:
+        print(result[1])
 
 while True:
     word = input("Word: ")
